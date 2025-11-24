a94f4397c1fdc8f38b9eb749eefb5c82918efaf2
thstkddnr20 <thstkddnr20@gmail.com>
2025-11-24 19:39:33 +0900

    CryptoUtil json 사용 X 역직렬화에서 문제가 생겨서 json으로 변환하지 않는다

--- src/main/java/com/nyangtodac/auth/util/CryptoUtil.java
+++ src/main/java/com/nyangtodac/auth/util/CryptoUtil.java
@@ -1,49 +1,36 @@
 package com.nyangtodac.auth.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
+import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.encrypt.BytesEncryptor;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Component
 public class CryptoUtil {
 
     private final BytesEncryptor encryptor;
-    private final ObjectMapper objectMapper;
 
     public CryptoUtil(
             @Value("${cookie.encryptor.password}") String password,
             @Value("${cookie.encryptor.salt}") String salt
     ) {
         this.encryptor = Encryptors.stronger(password, salt);
-        this.objectMapper = new ObjectMapper();
     }
 
     public String encrypt(OAuth2AuthorizationRequest authorizationRequest) {
-        String json;
-        try {
-            json = objectMapper.writeValueAsString(authorizationRequest);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        byte[] encrypted = encryptor.encrypt(json.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(encrypted);
+        byte[] serialized = SerializationUtils.serialize(authorizationRequest);
+        byte[] encrypted = encryptor.encrypt(serialized);
+        return Base64.getUrlEncoder().encodeToString(encrypted);
     }
 
     public OAuth2AuthorizationRequest decrypt(Cookie cookie) {
-        byte[] encrypted = Base64.getDecoder().decode(cookie.getValue());
+        byte[] encrypted = Base64.getUrlDecoder().decode(cookie.getValue());
         byte[] decrypted = encryptor.decrypt(encrypted);
-        try {
-            return objectMapper.readValue(new String(decrypted, StandardCharsets.UTF_8), OAuth2AuthorizationRequest.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return SerializationUtils.deserialize(decrypted);
     }
 }
