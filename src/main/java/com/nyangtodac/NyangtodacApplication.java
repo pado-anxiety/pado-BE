3d0992d4e01495fccc2970f59eb95412cc5d5342
thstkddnr20 <thstkddnr20@gmail.com>
2025-11-26 21:01:58 +0900

    스케줄링 도입

--- src/main/java/com/nyangtodac/NyangtodacApplication.java
+++ src/main/java/com/nyangtodac/NyangtodacApplication.java
@@ -2,8 +2,10 @@ package com.nyangtodac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NyangtodacApplication {
 
 	public static void main(String[] args) {
