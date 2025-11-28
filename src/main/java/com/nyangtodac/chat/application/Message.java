2f48260164a41f6585a7700b7db9a015097ebdf4
thstkddnr20 <thstkddnr20@gmail.com>
2025-11-28 21:15:47 +0900

    Message 생성 시 tsidUtil로 id 생성

--- src/main/java/com/nyangtodac/chat/application/Message.java
+++ src/main/java/com/nyangtodac/chat/application/Message.java
@@ -1,27 +1,26 @@
 package com.nyangtodac.chat.application;
 
 import com.nyangtodac.chat.controller.dto.Sender;
+import com.nyangtodac.chat.tsid.TsidUtil;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class Message {
 
+    private Long tsid;
     private String content;
     private String role;
-    private LocalDateTime createdAt;
 
     public Message(String content, String role) {
+        this.tsid = TsidUtil.generate();
         this.content = content;
         this.role = role.toUpperCase();
-        this.createdAt = LocalDateTime.now();
     }
 
-    public Message(String content, Sender sender, LocalDateTime localDateTime) {
+    public Message(Long tsid, String content, Sender sender) {
+        this.tsid = tsid;
         this.content = content;
         this.role = sender.getRole();
-        this.createdAt = localDateTime;
     }
 
     public Message() {
