a5897729b112dea17f4323344fb231833ab0070f
thstkddnr20 <thstkddnr20@gmail.com>
2025-12-03 15:52:36 +0900

    라이브러리 테스트 코드 CircuitBreakerTest 작성

---
+++ src/test/java/com/nyangtodac/resilience4j/circuitbreaker/DummyCircuitBreakerService.java
@@ -0,0 +1,51 @@
+package com.nyangtodac.resilience4j.circuitbreaker;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.Callable;
+
+import io.github.resilience4j.circuitbreaker.*;
+
+public class DummyCircuitBreakerService {
+
+    private final CircuitBreaker circuitBreaker;
+
+    public DummyCircuitBreakerService() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofSeconds(1))
+                .slidingWindowSize(2)
+                .permittedNumberOfCallsInHalfOpenState(2)
+                .recordExceptions(IOException.class)
+                .build();
+
+        //2번의 요청에서 (slidingWindowSize)
+        //실패율이 50이상일 경우 (failureRateThreshold)
+        //CircuitBreaker가 OPEN 된다
+        //OPEN되고 요청 없이 1초가 지나면 (waitDurationInOpenState)
+        //HALF_OPEN 상태가 된다
+        //HALF_OPEN 상태에서 2번의 요청이 성공하면 (permittedNumberOfCallsInHalfOpenState)
+        //CircuitBreaker가 CLOSED가 된다
+        this.circuitBreaker = CircuitBreaker.of("dummyService", config);
+    }
+
+    public String call(boolean fail) {
+        Callable<String> callable = CircuitBreaker.decorateCallable(circuitBreaker, () -> {
+            if (fail) {
+                throw new IOException("Dummy failure");
+            }
+            return "Success";
+        });
+
+        try {
+            return callable.call();
+        } catch (Exception e) {
+            return "Fallback response";
+        }
+    }
+
+
+    public CircuitBreaker getCircuitBreaker() {
+        return circuitBreaker;
+    }
+}
