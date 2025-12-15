c89a531777a77fbdb94d79a7c15d2abbe8415622
thstkddnr20 <thstkddnr20@gmail.com>
2025-12-15 23:57:14 +0900

    redis host, port 환경변수 주입

--- src/main/java/com/nyangtodac/external/redis/RedisConfig.java
+++ src/main/java/com/nyangtodac/external/redis/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.nyangtodac.external.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -10,8 +12,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port) {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
     @Bean
