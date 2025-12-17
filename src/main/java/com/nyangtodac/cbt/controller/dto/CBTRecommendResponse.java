641c3af5854b880e69bec3d9ddfdab91c11c0951
손상욱 <thstkddnr20@naver.com>
2025-12-18 01:16:06 +0900

    cbt 추천 메시지 배열 방식으로 변경

--- src/main/java/com/nyangtodac/cbt/controller/dto/CBTRecommendResponse.java
+++ src/main/java/com/nyangtodac/cbt/controller/dto/CBTRecommendResponse.java
@@ -3,13 +3,15 @@ package com.nyangtodac.cbt.controller.dto;
 import com.nyangtodac.cbt.recommend.CBT;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class CBTRecommendResponse {
     private final CBT cbt;
-    private final String message;
+    private final List<String> messages;
 
-    public CBTRecommendResponse(CBT cbt, String message) {
+    public CBTRecommendResponse(CBT cbt, List<String> messages) {
         this.cbt = cbt;
-        this.message = message;
+        this.messages = messages;
     }
 }
