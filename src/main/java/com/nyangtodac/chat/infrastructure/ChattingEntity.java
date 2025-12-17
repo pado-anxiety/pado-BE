580f69620124a225f59715e3a81aa6b67a25cc86
손상욱 <thstkddnr20@naver.com>
2025-12-18 01:06:52 +0900

    ChattingEntity 저장 방식 변경 MessageEntity로부터 이름 변경 jsonPayload를 저장하도록 수정

---
+++ src/main/java/com/nyangtodac/chat/infrastructure/ChattingEntity.java
@@ -0,0 +1,29 @@
+package com.nyangtodac.chat.infrastructure;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "chatting")
+@Getter
+public class ChattingEntity {
+
+    @Id
+    private Long tsid;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Lob
+    @Column(nullable = false)
+    private String jsonPayload;
+
+    protected ChattingEntity() {
+    }
+
+    public ChattingEntity(Long tsid, Long userId, String jsonPayload) {
+        this.tsid = tsid;
+        this.userId = userId;
+        this.jsonPayload = jsonPayload;
+    }
+}
