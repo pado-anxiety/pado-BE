5013a0d57f67c96fe6d1708bddd0deae337f662b
손상욱 <thstkddnr20@naver.com>
2025-12-25 21:08:03 +0900

    db flush async 설정 변경

--- src/main/java/com/nyangtodac/config/AsyncConfig.java
+++ src/main/java/com/nyangtodac/config/AsyncConfig.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
@@ -13,10 +14,13 @@ public class AsyncConfig {
     @Bean(name = "dbTaskExecutor")
     public Executor dbTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(50);
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(200);
         executor.setThreadNamePrefix("db-flush-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(10);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
