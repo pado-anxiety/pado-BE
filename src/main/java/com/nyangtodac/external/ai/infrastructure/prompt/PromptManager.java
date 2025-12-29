1e135c7b8dd386504666cc56c9daf2a87d17a590
손상욱 <thstkddnr20@naver.com>
2025-12-29 20:12:10 +0900

    ACT 추천 프롬프트 추가

--- src/main/java/com/nyangtodac/external/ai/infrastructure/prompt/PromptManager.java
+++ src/main/java/com/nyangtodac/external/ai/infrastructure/prompt/PromptManager.java
@@ -10,11 +10,13 @@ public class PromptManager {
     private final SystemPrompt chatSystemPrompt;
     private final SystemPrompt summarySystemPrompt;
     private final SystemPrompt summaryPrefix;
+    private final SystemPrompt actRecommendPrompt;
 
     public PromptManager() {
         chatSystemPrompt = YamlResourceLoader.load("prompts/chat_prompt.yaml", SystemPrompt.class);
         summarySystemPrompt = YamlResourceLoader.load("prompts/summary_prompt.yaml", SystemPrompt.class);
         summaryPrefix = YamlResourceLoader.load("prompts/summary_prefix.yaml", SystemPrompt.class);
+        actRecommendPrompt = YamlResourceLoader.load("prompts/act_recommend_prompt.yaml", SystemPrompt.class);
     }
 
 }
