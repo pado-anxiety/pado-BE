84437aae4eea0f5578007e7eccd54c55e60edfd5
손상욱 <thstkddnr20@naver.com>
2025-12-30 13:37:09 +0900

    objectmapper 주입, base64 인코딩 제거

--- src/main/java/com/nyangtodac/chat/infrastructure/ChattingSerializer.java
+++ src/main/java/com/nyangtodac/chat/infrastructure/ChattingSerializer.java
@@ -6,24 +6,19 @@ import com.nyangtodac.chat.domain.Chatting;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ChattingSerializer {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     public String serialize(Chatting chatting) throws JsonProcessingException {
-        String json = objectMapper.writeValueAsString(chatting);
-        return Base64.getEncoder().encodeToString(json.getBytes(StandardCharsets.UTF_8));
+        return objectMapper.writeValueAsString(chatting);
     }
 
-    public Optional<Chatting> deserialize(String base64) throws JsonProcessingException {
-        byte[] decoded = Base64.getDecoder().decode(base64);
-        String json = new String(decoded, StandardCharsets.UTF_8);
+    public Optional<Chatting> deserialize(String json) throws JsonProcessingException {
         return Optional.of(objectMapper.readValue(json, Chatting.class));
     }
 
