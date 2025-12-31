e53a6ad11b5470bcc6e53c9452730ef666c2058e
thstkddnr20 <thstkddnr20@gmail.com>
2025-12-31 20:01:24 +0900

    비동기 방식 저장 제거, 채팅 저장 메서드 생성

--- src/main/java/com/nyangtodac/chat/infrastructure/ChattingDBRepository.java
+++ src/main/java/com/nyangtodac/chat/infrastructure/ChattingDBRepository.java
@@ -3,14 +3,14 @@ package com.nyangtodac.chat.infrastructure;
 import com.nyangtodac.chat.domain.Chatting;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public interface ChattingDBRepository {
+
+    Chatting save(Long userId, Chatting chatting);
+
     List<Chatting> findRecentChattingsLessThanCursor(Long userId, Long cursor, int n);
 
     List<Chatting> findRecentMessages(Long userId, int n);
-
-    CompletableFuture<Void> asyncBatch(Long userId, List<Chatting> chattings);
 
     List<Chatting> findChattingsAfterTsidOrderByTsidAsc(Long userId, Long latestTsid, int limit);
 }
--- src/main/java/com/nyangtodac/chat/infrastructure/ChattingDBRepositoryImpl.java
+++ src/main/java/com/nyangtodac/chat/infrastructure/ChattingDBRepositoryImpl.java
@@ -4,11 +4,9 @@ import com.nyangtodac.chat.domain.Chatting;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +16,11 @@ public class ChattingDBRepositoryImpl implements ChattingDBRepository {
     private final ChattingJpaRepository chattingJpaRepository;
 
     @Override
+    public Chatting save(Long userId, Chatting chatting) {
+        return chattingJpaRepository.save(ChattingEntity.fromModel(userId, chatting)).toModel();
+    }
+
+    @Override
     public List<Chatting> findRecentChattingsLessThanCursor(Long userId, Long cursor, int n) {
         return chattingJpaRepository.findByUserIdAndTsidLessThanOrderByTsidDesc(userId, cursor, PageRequest.of(0, n)).stream().map(ChattingEntity::toModel).toList();
     }
@@ -25,17 +28,6 @@ public class ChattingDBRepositoryImpl implements ChattingDBRepository {
     @Override
     public List<Chatting> findRecentMessages(Long userId, int n) {
         return chattingJpaRepository.findByUserIdOrderByTsidDesc(userId, PageRequest.of(0, n)).stream().map(ChattingEntity::toModel).toList();
-    }
-
-    @Override
-    @Async("dbTaskExecutor")
-    public CompletableFuture<Void> asyncBatch(Long userId, List<Chatting> chattings) {
-        return CompletableFuture.runAsync(() -> {
-            if (!chattings.isEmpty()) {
-                List<ChattingEntity> entities = chattings.stream().map(c -> ChattingEntity.fromModel(userId, c)).toList();
-                chattingJpaRepository.saveAll(entities);
-            }
-        });
     }
 
     @Override
