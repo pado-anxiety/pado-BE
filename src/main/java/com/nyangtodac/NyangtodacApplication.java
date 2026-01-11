e61bbaa52649af7716d31dced01acb4844f37ca2
손상욱 <thstkddnr20@naver.com>
2026-01-11 20:43:34 +0900

    스케줄링 사용 코드 제거

--- src/main/java/com/nyangtodac/NyangtodacApplication.java
+++ src/main/java/com/nyangtodac/NyangtodacApplication.java
@@ -2,10 +2,8 @@ package com.nyangtodac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class NyangtodacApplication {
 
 	public static void main(String[] args) {
