c31442943d14b82792f0445f93b44094e577bf33
thstkddnr20 <thstkddnr20@gmail.com>
2026-01-12 17:25:23 +0900

    의도가 명확하지 않은 테스트 코드 제거

--- src/test/java/com/pado/tsid/TsidFactoryTest.java
+++ src/test/java/com/pado/tsid/TsidFactoryTest.java
@@ -11,7 +11,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TsidFactoryTest {
 
@@ -40,33 +39,5 @@ public class TsidFactoryTest {
         es.awaitTermination(5, TimeUnit.SECONDS);
 
         assertEquals(20_000, set.size());
-    }
-
-    @Test
-    @DisplayName("같은 노드 값으로 설정된 TSID는 중복될 수 있음")
-    void collision_between_same_nodes() throws Exception {
-        TSID.Factory f1 = TSID.Factory.builder().withNode(1).build();
-        TSID.Factory f2 = TSID.Factory.builder().withNode(1).build();
-        //node 모두 1
-
-        Set<TSID> set = ConcurrentHashMap.newKeySet();
-        ExecutorService es = Executors.newFixedThreadPool(2);
-
-        es.submit(() -> {
-            for (int i = 0; i < 10_000; i++) {
-                set.add(f1.generate());
-            }
-        });
-
-        es.submit(() -> {
-            for (int i = 0; i < 10_000; i++) {
-                set.add(f2.generate());
-            }
-        });
-
-        es.shutdown();
-        es.awaitTermination(5, TimeUnit.SECONDS);
-
-        assertNotEquals(20_000, set.size());
     }
 }
