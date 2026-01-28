fa7fd7447caccd21912f83a55f2af60e2ec78c94
손상욱 <thstkddnr20@naver.com>
2026-01-29 06:39:31 +0900

    OAuthService를 각 oauth에 맞는 서비스로 분리

--- src/main/java/com/pado/auth/controller/LoginController.java
+++ src/main/java/com/pado/auth/controller/LoginController.java
@@ -5,7 +5,10 @@ import com.pado.auth.controller.dto.GoogleLoginRequest;
 import com.pado.auth.controller.dto.KakaoLoginRequest;
 import com.pado.auth.controller.dto.TokenResponse;
 import com.pado.auth.infrastructure.LoginUser;
-import com.pado.auth.service.OAuth2Service;
+import com.pado.auth.infrastructure.oauth.apple.AppleOAuthService;
+import com.pado.auth.infrastructure.oauth.google.GoogleOAuthService;
+import com.pado.auth.infrastructure.oauth.kakao.KakaoOAuthService;
+import com.pado.auth.service.LogoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,26 +19,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LoginController {
 
-    private final OAuth2Service oAuth2Service;
+    private final GoogleOAuthService googleOAuthService;
+    private final KakaoOAuthService kakaoOAuthService;
+    private final AppleOAuthService appleOAuthService;
+
+    private final LogoutService logoutService;
 
     @PostMapping("/login/google")
     public ResponseEntity<TokenResponse> googleLogin(@RequestBody GoogleLoginRequest loginRequest) {
-        return ResponseEntity.ok(oAuth2Service.googleLogin(loginRequest.getAuthorizationCode(), loginRequest.getCodeVerifier(), loginRequest.getRedirectUri(), loginRequest.getPlatform()));
+        return ResponseEntity.ok(googleOAuthService.googleLogin(loginRequest.getAuthorizationCode(), loginRequest.getCodeVerifier(), loginRequest.getRedirectUri(), loginRequest.getPlatform()));
     }
 
     @PostMapping("/login/kakao")
     public ResponseEntity<TokenResponse> kakaoLogin(@RequestBody KakaoLoginRequest loginRequest) {
-        return ResponseEntity.ok(oAuth2Service.kakaoLogin(loginRequest.getAccessToken()));
+        return ResponseEntity.ok(kakaoOAuthService.kakaoLogin(loginRequest.getIdentityToken()));
     }
 
     @PostMapping("/login/apple")
     public ResponseEntity<TokenResponse> appleLogin(@RequestBody AppleLoginRequest loginRequest) {
-        return ResponseEntity.ok(oAuth2Service.appleLogin(loginRequest.getAuthorizationCode(), loginRequest.getFullName()));
+        return ResponseEntity.ok(appleOAuthService.appleLogin(loginRequest.getAuthorizationCode(), loginRequest.getFullName()));
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(@LoginUser Long userId) {
-        oAuth2Service.logout(userId);
+        logoutService.logout(userId);
         return ResponseEntity.noContent().build();
     }
 }
