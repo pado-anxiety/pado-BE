7cfbda88b3199388a6b188579cecbdec7dab9f78
손상욱 <thstkddnr20@naver.com>
2026-02-13 15:21:07 +0900

    firebase 엔티티 작성

---
+++ src/main/java/com/pado/fcm/infrastructure/FcmEntity.java
@@ -0,0 +1,39 @@
+package com.pado.fcm.infrastructure;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "fcm")
+@Getter
+public class FcmEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String token;
+
+    private Boolean isActive;
+
+    private Instant updatedAt;
+
+    protected FcmEntity() {
+    }
+
+    private FcmEntity(Long id, Long userId, String token, Boolean isActive, Instant updatedAt) {
+        this.id = id;
+        this.userId = userId;
+        this.token = token;
+        this.isActive = isActive;
+        this.updatedAt = updatedAt;
+    }
+
+    public FcmEntity(Long userId, String token) {
+        this(null, userId, token, true, Instant.now());
+    }
+}
