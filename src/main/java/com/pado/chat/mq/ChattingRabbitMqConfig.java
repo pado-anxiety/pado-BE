392e188bdb5f3a0e3f02d4a56cbe5454e44ee99d
손상욱 <thstkddnr20@naver.com>
2026-02-19 12:00:10 +0900

    failed consumer 제거 flushConsumer에서 실패하면 바로 dead queue로 이동

--- src/main/java/com/pado/chat/mq/ChattingRabbitMqConfig.java
+++ src/main/java/com/pado/chat/mq/ChattingRabbitMqConfig.java
@@ -15,7 +15,6 @@ public class ChattingRabbitMqConfig {
 
     public static final String EXCHANGE = "chat.flush.ex";
     public static final String FLUSH_QUEUE = "chat.flush";
-    public static final String FAILED_QUEUE = "chat.failed";
     public static final String DEAD_QUEUE = "chat.dead";
 
     @Bean
@@ -57,14 +56,6 @@ public class ChattingRabbitMqConfig {
     public Queue flushQueue() {
         return QueueBuilder.durable(FLUSH_QUEUE)
                 .deadLetterExchange(EXCHANGE)
-                .deadLetterRoutingKey(FAILED_QUEUE)
-                .build();
-    }
-
-    @Bean
-    public Queue failedQueue() {
-        return QueueBuilder.durable(FAILED_QUEUE)
-                .deadLetterExchange(EXCHANGE)
                 .deadLetterRoutingKey(DEAD_QUEUE)
                 .build();
     }
@@ -79,13 +70,6 @@ public class ChattingRabbitMqConfig {
         return BindingBuilder.bind(flushQueue())
                 .to(chatExchange())
                 .with(FLUSH_QUEUE);
-    }
-
-    @Bean
-    public Binding failedBinding() {
-        return BindingBuilder.bind(failedQueue())
-                .to(chatExchange())
-                .with(FAILED_QUEUE);
     }
 
     @Bean
