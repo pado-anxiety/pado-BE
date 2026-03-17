20e4e81cc680f0325a9c3a6e673bffb3a5a8736f
thstkddnr20 <thstkddnr20@gmail.com>
2026-03-17 21:57:44 +0900

    master 브랜치 파일 복구

--- src/main/java/com/pado/chat/application/AIChatFacade.java
+++ src/main/java/com/pado/chat/application/AIChatFacade.java
@@ -4,6 +4,8 @@ import com.pado.chat.application.command.PostMessageResult;
 import com.pado.chat.controller.dto.Sender;
 import com.pado.chat.controller.dto.message.MessageRequest;
 import com.pado.chat.domain.Chatting;
+import com.pado.chat.quota.ChatQuotaExceededException;
+import com.pado.chat.quota.QuotaStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,13 @@ public class AIChatFacade {
     private final ChatPreProcessor chatPreProcessor;
     private final ChatPostProcessor chatPostProcessor;
     private final AIChatService aiChatService;
+    private final AIChatQuotaService aiQuotaService;
 
     public PostMessageResult postMessageV1(Long userId, MessageRequest messageRequest) {
-//        if (!aiQuotaService.tryConsume(userId)) {
-//            QuotaStatus quotaStatus = aiQuotaService.getQuotaStatus(userId);
-//            throw new ChatQuotaExceededException(quotaStatus);
-//        } FIXME
+        if (!aiQuotaService.tryConsume(userId)) {
+            QuotaStatus quotaStatus = aiQuotaService.getQuotaStatus(userId);
+            throw new ChatQuotaExceededException(quotaStatus);
+        }
         Chatting userChatting = new Chatting(messageRequest.getMessage(), Sender.USER);
         ChatPreProcessResult preProcessResult = chatPreProcessor.preProcess(userId, userChatting);
         Chatting reply = aiChatService.postMessage(preProcessResult);
