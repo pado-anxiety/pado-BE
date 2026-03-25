3094af57762f9df99a996c8c18d2d85763fa3419
thstkddnr20 <thstkddnr20@gmail.com>
2026-03-25 16:12:55 +0900

    CorrelationData를 사용하여 Publisher Confirm 진행

--- src/main/java/com/pado/chat/mq/ChattingFlushProducer.java
+++ src/main/java/com/pado/chat/mq/ChattingFlushProducer.java
@@ -3,10 +3,16 @@ package com.pado.chat.mq;
 import com.pado.chat.domain.Chatting;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.springframework.amqp.rabbit.connection.CorrelationData.Confirm;
 
 @Service
 @RequiredArgsConstructor
@@ -16,17 +22,27 @@ public class ChattingFlushProducer {
     private final RabbitTemplate rabbitTemplate;
 
     public boolean publish(Long userId, List<Chatting> chattings) {
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
         try {
-            for (Chatting chatting : chattings) {
-                rabbitTemplate.convertAndSend(
-                        ChattingRabbitMqConfig.EXCHANGE,
-                        ChattingRabbitMqConfig.FLUSH_QUEUE,
-                        new ChattingPersistMessage(userId, chatting)
-                );
+            rabbitTemplate.convertAndSend(
+                    ChattingRabbitMqConfig.EXCHANGE,
+                    ChattingRabbitMqConfig.FLUSH_QUEUE,
+                    new ChattingPersistMessage(userId, chattings),
+                    correlationData
+            );
+
+            Confirm confirm = correlationData.getFuture().get(5, TimeUnit.SECONDS);
+
+            if (!confirm.isAck()) {
+                log.error("MQ publish nack. cause={}", confirm.getReason());
+                return false;
             }
             return true;
+        } catch (TimeoutException e) {
+            log.error("MQ Publish timeout. userId={}", userId, e);
+            return false;
         } catch (Exception e) {
-            log.error("chat flush failed. userId={}", userId, e);
+            log.error("MQ Publish failed with exception. userId={}", userId, e);
             return false;
         }
     }
