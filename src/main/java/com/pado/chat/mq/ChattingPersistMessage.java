8ed54d5c2b0d0b0472dfeb7faa5a40318d3788da
thstkddnr20 <thstkddnr20@gmail.com>
2026-03-25 16:12:17 +0900

    MQ 메시지에 사용자, AI 응답을 하나의 단위로 포함

--- src/main/java/com/pado/chat/mq/ChattingPersistMessage.java
+++ src/main/java/com/pado/chat/mq/ChattingPersistMessage.java
@@ -2,16 +2,20 @@ package com.pado.chat.mq;
 
 import com.pado.chat.domain.Chatting;
 import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
 
 @Getter
+@ToString
 public class ChattingPersistMessage {
 
     private Long userId;
-    private Chatting chatting;
+    private List<Chatting> chattings;
 
-    public ChattingPersistMessage(Long userId, Chatting chatting) {
+    public ChattingPersistMessage(Long userId, List<Chatting> chattings) {
         this.userId = userId;
-        this.chatting = chatting;
+        this.chattings = chattings;
     }
 
     public ChattingPersistMessage() {
